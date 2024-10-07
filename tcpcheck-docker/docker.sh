3d8cc1b11b72caa9a02e309661f5a36f2ae2dbac
Jayesh <jayesh@pritis-mbp.lan>
2024-10-07 17:26:05 -0400

    new changes for local

--- tcpcheck-docker/docker.sh
+++ tcpcheck-docker/docker.sh
@@ -1,8 +1,8 @@
-docker stop tcpcheck-container
-docker rm tcpcheck-container
-docker build -t tcpcheck-docker .
-docker run -d -p 8080:8080 --name tcpcheck-container tcpcheck-docker
+docker stop devops-cloud-api-container
+docker rm devops-cloud-api-container
+docker build -t devops-cloud-api-docker .
+docker run -d -p 8080:8080 --name devops-cloud-api-container devops-cloud-api-docker
 docker ps
-# docker tag tcpcheck-docker jayeshmahajan/tcpcheck:v0.0.1
-# docker tag jayeshmahajan/tcpcheck:v0.0.1 jayeshmahajan/tcpcheck:latest
-# docker push jayeshmahajan/tcpcheck:latest
+# docker tag devops-cloud-api-docker jayeshmahajan/devops-cloud-api:v0.0.1
+# docker tag jayeshmahajan/devops-cloud-api:v0.0.1 jayeshmahajan/devops-cloud-api:latest
+# docker push jayeshmahajan/devops-cloud-api:latest
